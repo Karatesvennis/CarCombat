609523ecba24cc494e1e0df14098c9324e5819d2
Petter Svensson <petter.svensson.90@gmail.com>
2019-04-10 13:22:01 +0200

    Cameracontroller and playercontroller added

---
+++ Car Combat/Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    [SerializeField] float turnSpeed = 4f;
+    
+    //float horizontalSpeed = 2f;
+    //float verticalSpeed = 2f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector3(player.transform.position.x, player.transform.position.y + 2f, player.transform.position.z - 4f);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
+
+        transform.position = player.transform.position + offset;
+        transform.LookAt(player.transform.position);
+    }
+}
---
+++ Car Combat/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] float speed = 10f;
+    [SerializeField] float rotationSpeed = 100f;
+
+    private Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+    }
+}
