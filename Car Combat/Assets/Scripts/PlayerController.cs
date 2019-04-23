fbff79dc0135e81e260a46b18b3a92c3984ac57a
Petter Svensson <petter.svensson.90@gmail.com>
2019-04-23 16:02:09 +0200

    In progress regarding collision.

--- Car Combat/Assets/Scripts/PlayerController.cs
+++ Car Combat/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float speed = 10f;
     [SerializeField] float rotationSpeed = 100f;
     [SerializeField] float bumpForce = 10f;
+    [SerializeField] float maxSpeed = 10f;
+    [SerializeField] float maxRotation = 50f;
 
+    float newSpeedForce;
+
+    private Vector3 eulerAngleVelocity;
     private Rigidbody rb;
 
     bool dead = false;
@@ -22,7 +27,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float translation = Input.GetAxis("Vertical") * speed;
+
+        Move();
+        GetComponent<Fire>().FireProjectile();
+
+
+        /*float translation = Input.GetAxis("Vertical") * speed;
         float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
 
         translation *= Time.deltaTime;
@@ -31,21 +41,78 @@ public class PlayerController : MonoBehaviour
         transform.Translate(0, 0, translation);
         transform.Rotate(0, rotation, 0);
 
-        GetComponent<Fire>().FireProjectile();
+        float moveVertical = Input.GetAxis("Vertical");
+        float turn = Input.GetAxis("Horizontal");
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        Vector3 movement = new Vector3(turn, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+        rb.rotation += rotation;*/
     }
 
-    /*private void OnCollisionEnter(Collision other)
+    private void Move()
+    {
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = transform.forward * moveVertical;
+        rb.AddForce(movement * speed);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+
+        var xRotation = rb.rotation.x;
+        var yRotation = rb.rotation.y;
+        var zRotation = rb.rotation.z;
+        
+        eulerAngleVelocity = new Vector3(0, Input.GetAxis("Horizontal"), 0);
+        Quaternion deltaRotation = Quaternion.Euler((eulerAngleVelocity * rotationSpeed) * Time.deltaTime);
+        rb.rotation *= deltaRotation;
+
+        //yRotation = Mathf.Clamp(yRotation, -90, 90);
+        //xRotation = Mathf.Clamp(xRotation, 0, 0);
+        //zRotation = Mathf.Clamp(zRotation, 0, 0);
+    }
+
+    private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Enemy")
         {
-            Debug.Log("Bumped enemy");
 
-            //Rigidbody enemyRb = other.gameObject.GetComponent<Rigidbody>();
-
-            Vector3 angle = other.contacts[0].point - transform.position;
-            angle = -angle.normalized;
-            rb.AddForce(angle * bumpForce);
+            Rigidbody enemyRb = other.gameObject.GetComponent<Rigidbody>();
             
+
+            if (SpeedCollisionCheck(rb, enemyRb))
+            {
+                Debug.Log("Player was faster");
+                Vector3 otherAngle = other.contacts[0].point - other.transform.position;
+                otherAngle = -otherAngle.normalized;
+                enemyRb.AddForce((otherAngle * bumpForce) * newSpeedForce);
+                //Debug.Log(newSpeedForce);
+            }
+            else if (!SpeedCollisionCheck(rb, enemyRb))
+            {
+                Debug.Log("Enemy was faster");
+                Vector3 angle = other.contacts[0].point - transform.position;
+                angle = -angle.normalized;
+                rb.AddForce((angle * bumpForce) * newSpeedForce);
+                //Debug.Log(newSpeedForce);
+            }
         }
-    }*/
+    }
+
+    bool SpeedCollisionCheck(Rigidbody player, Rigidbody enemy)
+    {
+        var speedDifference = player.velocity.sqrMagnitude - enemy.velocity.sqrMagnitude;
+        var nextSpeedDifference = enemy.velocity.sqrMagnitude - player.velocity.sqrMagnitude;
+
+        //Debug.Log(speedDifference + " " + nextSpeedDifference);
+
+        if (speedDifference > nextSpeedDifference)
+        {
+            newSpeedForce = speedDifference;
+            return true;
+        }
+        else
+        {
+            newSpeedForce = nextSpeedDifference;
+            return false;
+        }
+    }
 }
