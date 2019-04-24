6ffe23df100e7b009367f8dff208a2b235e31d3f
Petter Svensson <petter.svensson.90@gmail.com>
2019-04-24 16:44:34 +0200

    Fixed player movement so it works better

--- Car Combat/Assets/Scripts/PlayerController.cs
+++ Car Combat/Assets/Scripts/PlayerController.cs
@@ -6,11 +6,13 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] float speed = 10f;
+    [SerializeField] float sideSpeed = 5f;
     [SerializeField] float rotationSpeed = 100f;
     [SerializeField] float bumpForce = 10f;
     [SerializeField] float bumpExplosionRaduis = 2f;
     [SerializeField] float maxSpeed = 10f;
-    [SerializeField] float maxRotation = 50f;
+
+    float yRotation = 0f;
 
     float bumpDamage = 200f;
     float newSpeedForce;
@@ -29,47 +31,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         Move();
         GetComponent<Fire>().FireProjectile();
-
-
-        /*float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-
-        transform.Translate(0, 0, translation);
-        transform.Rotate(0, rotation, 0);
-
-        float moveVertical = Input.GetAxis("Vertical");
-        float turn = Input.GetAxis("Horizontal");
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-
-        Vector3 movement = new Vector3(turn, 0.0f, moveVertical);
-        rb.AddForce(movement * speed);
-        rb.rotation += rotation;*/
     }
 
     private void Move()
     {
+        //Drives the car
         float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = transform.forward * moveVertical;
-        rb.AddForce(movement * speed);
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 verticalMovement = transform.forward * moveVertical;
+        Vector3 horizontalMovement = transform.right * moveHorizontal;
+        rb.AddForce(verticalMovement * speed);
+        rb.AddForce(horizontalMovement * sideSpeed);
         rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
 
-        var xRotation = rb.rotation.x;
-        var yRotation = rb.rotation.y;
-        var zRotation = rb.rotation.z;
-        
+        //Rotates the car
         eulerAngleVelocity = new Vector3(0, Input.GetAxis("Horizontal"), 0);
         Quaternion deltaRotation = Quaternion.Euler((eulerAngleVelocity * rotationSpeed) * Time.deltaTime);
         rb.rotation *= deltaRotation;
-
-        //yRotation = Mathf.Clamp(yRotation, -90, 90);
-        //xRotation = Mathf.Clamp(xRotation, 0, 0);
-        //zRotation = Mathf.Clamp(zRotation, 0, 0);
     }
 
     private void OnTriggerEnter(Collider other)
