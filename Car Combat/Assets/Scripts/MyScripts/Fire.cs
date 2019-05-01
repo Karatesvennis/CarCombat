63ca7ffa40399fa6e2651afd8060d1c0d141ef68
Petter Svensson <petter.svensson.90@gmail.com>
2019-05-01 17:31:11 +0200

    Started working on ammo/weapon/pickups

--- Car Combat/Assets/Scripts/MyScripts/Fire.cs
+++ Car Combat/Assets/Scripts/MyScripts/Fire.cs
@@ -11,31 +11,49 @@ public class Fire : MonoBehaviour
     [SerializeField] float speed;
     [SerializeField] float fireRate;
     float nextFire;
+    int amountOfAmmo = 2;
+
+    GameObject crosshair;
 
     Vector3 hitPoint;
+
+    private void Start()
+    {
+        crosshair = FindObjectOfType<Crosshair>().gameObject;
+    }
 
 
     public void FireProjectile()
     {
-        Ray ray;
-        RaycastHit hit;
-
-        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        if (amountOfAmmo <= 0)
         {
-            nextFire = Time.time + fireRate;
-            Vector3 mouseResult = Input.mousePosition;
-            mouseResult = new Vector3(Screen.width / 2, mouseResult.y);
+            crosshair.SetActive(false);
+        }
+        else
+        {
+            crosshair.SetActive(true);
 
-            ray = mainCam.ScreenPointToRay(mouseResult);
+            Ray ray;
+            RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit))
+            if (Input.GetButton("Fire1") && Time.time > nextFire)
             {
-                hitPoint = hit.point;
-                Vector3 moveDirection = (hitPoint - gun.transform.position).normalized;
-                GameObject newProjectile = Instantiate(projectile, gun.transform.position, Quaternion.identity);
-                newProjectile.GetComponent<Projectile>().playerShot = true;
-                Rigidbody rb = newProjectile.GetComponent<Rigidbody>();
-                rb.velocity = moveDirection * speed;
+                nextFire = Time.time + fireRate;
+                Vector3 mouseResult = Input.mousePosition;
+                mouseResult = new Vector3(Screen.width / 2, mouseResult.y);
+
+                ray = mainCam.ScreenPointToRay(mouseResult);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    hitPoint = hit.point;
+                    Vector3 moveDirection = (hitPoint - gun.transform.position).normalized;
+                    GameObject newProjectile = Instantiate(projectile, gun.transform.position, Quaternion.identity);
+                    newProjectile.GetComponent<Projectile>().playerShot = true;
+                    Rigidbody rb = newProjectile.GetComponent<Rigidbody>();
+                    rb.velocity = moveDirection * speed;
+                    amountOfAmmo--;
+                }
             }
         }
     }
