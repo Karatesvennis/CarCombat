cf54929097640166d79f22f41cfa06b478eaa901
Petter Svensson <petter.svensson.90@gmail.com>
2019-05-01 12:03:02 +0200

    Added bigger expl, slow motion. Physics don't work after slow mo

--- Car Combat/Assets/Scripts/MyScripts/Projectile.cs
+++ Car Combat/Assets/Scripts/MyScripts/Projectile.cs
@@ -10,22 +10,29 @@ public class Projectile : MonoBehaviour
 
     public bool playerShot;
 
+    TimeFlowManager timeManager;
+
+
+    private void Start()
+    {
+        timeManager = FindObjectOfType<TimeFlowManager>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
 
         if (playerShot && other.tag != "Player" && other.GetType() != typeof(SphereCollider))
         {
+            ExplosionDamageOnImpact(transform.position, 30);
+
             if (other.GetComponent<Health>())
             {
                 other.GetComponent<Health>().DealDamage(damage);
-                DestroyWithVFX();
             }
-            else
-            {
-                Destroy(gameObject);
-            }
+            DestroyWithVFX();
+            timeManager.DoSlowMotion();
         }
+
         else if (!playerShot && other.tag != "Enemy" && other.GetType() != typeof(SphereCollider))
         {
             if (other.GetComponent<Health>())
@@ -40,10 +47,25 @@ public class Projectile : MonoBehaviour
         }
     }
 
+    void ExplosionDamageOnImpact(Vector3 center, float radius)
+    {
+
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            Rigidbody hitRb = hitColliders[i].gameObject.GetComponent<Rigidbody>();
+            if (hitRb != null)
+            {
+                hitRb.AddExplosionForce(5f, center, 8f, 3f, ForceMode.Impulse);
+            }
+        }
+    }
+
     void DestroyWithVFX()
     {
-        GameObject myDeathVFX = Instantiate(deathVFX, this.transform.position, Quaternion.identity);
-        Destroy(myDeathVFX, 0.2f);
+
+        GameObject myDeathVFX = Instantiate(deathVFX, this.transform.position + Vector3.up, Quaternion.identity);
+        Destroy(myDeathVFX, 1f);
         Destroy(gameObject);
     }
 }
---
+++ Car Combat/Assets/Scripts/MyScripts/TimeFlowManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeFlowManager : MonoBehaviour
+{
+
+    [SerializeField] float slowDownFactor = 0.05f;
+    [SerializeField] float slowDownLength = 2f;
+
+    
+    void Update()
+    {
+        Time.timeScale += (1f / slowDownLength) * Time.unscaledDeltaTime;
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+        Debug.Log(Time.timeScale);
+    }
+
+    public void DoSlowMotion()
+    {
+        Time.timeScale = slowDownFactor;
+        Time.fixedDeltaTime = Time.timeScale * 0.02f;
+    }
+}
