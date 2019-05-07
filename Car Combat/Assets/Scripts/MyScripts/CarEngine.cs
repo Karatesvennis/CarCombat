0b7d38a30e917f5f0727a996cb5624cf78def4d5
Petter Svensson <petter.svensson.90@gmail.com>
2019-05-07 15:04:05 +0200

    More work on new AI car

--- Car Combat/Assets/Scripts/MyScripts/CarEngine.cs
+++ Car Combat/Assets/Scripts/MyScripts/CarEngine.cs
@@ -9,20 +9,36 @@ public class CarEngine : MonoBehaviour
     private Transform path;
     private List<Transform> nodes;
     private int currentNode = 0;
+    private Rigidbody rb;
 
+    public bool isBraking = false;
+    public Vector3 centerOfMass;
+    
+
+    [Header("Wheel Colliders")]
     public WheelCollider wheelFL;
     public WheelCollider wheelFR;
     public WheelCollider wheelBL;
     public WheelCollider wheelBR;
 
-    public float maxSteerAngle = 40f;
+    [Header("Drive values")]
+    public float maxSteerAngle = 45f;
     public float maxMotorTorque = 500f;
+    public float maxBrakeTorque = 800f;
     public float currentSpeed;
     public float maxSpeed = 100f;
+
+    [Header("Sensors")]
+    public float sensorLength = 5f;
+    public float frontSensorPosition = 0.5f;
+    public float sideSensorPosition = 0.2f;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = centerOfMass;
         path = FindObjectOfType<Path>().transform;
         Transform[] pathTransforms = path.GetComponentsInChildren<Transform>();
         nodes = new List<Transform>();
@@ -39,16 +55,31 @@ public class CarEngine : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        Sensors();
         Drive();
+        Braking();
         ApplySteer();
         CheckWaypointDistance();
+    }
+
+    private void Sensors()
+    {
+        RaycastHit hit;
+        Vector3 sensorStartPos = transform.position;
+        sensorStartPos.z += frontSensorPosition;
+
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
+        {
+            Debug.DrawLine(sensorStartPos, hit.point, Color.white);
+        }
+        
     }
 
     private void Drive()
     {
         currentSpeed = 2 * Mathf.PI * wheelFL.radius * wheelFL.rpm * 60 / 1000;
 
-        if (currentSpeed < maxSpeed)
+        if (currentSpeed < maxSpeed && !isBraking)
         {
             wheelFL.motorTorque = maxMotorTorque;
             wheelFR.motorTorque = maxMotorTorque;
@@ -57,6 +88,20 @@ public class CarEngine : MonoBehaviour
         {
             wheelFL.motorTorque = 0;
             wheelFR.motorTorque = 0;
+        }
+    }
+
+    private void Braking()
+    {
+        if (isBraking)
+        {
+            wheelBL.brakeTorque = maxBrakeTorque;
+            wheelBR.brakeTorque = maxBrakeTorque;
+        }
+        else
+        {
+            wheelBL.brakeTorque = 0;
+            wheelBR.brakeTorque = 0;
         }
     }
 
